2c95985e3ac837840fe13ccc881acb62ac28c182
bCubed3 <sf.bcubed@gmail.com>
2020-01-10 12:00:42 -0800

    Fixed grav mechanics, more realistic mass
    need to fix the grav constant

--- body.py
+++ body.py
@@ -2,23 +2,23 @@ from vector import Vector
 import pygame
 
 class Body:
-    def __init__(self, mass, size, color, pos, velocity):
+    def __init__(self, mass, size, color, pos, velocity, sim_speed):
         self.mass = mass
         self.size = size
         self.color = color
         self.pos = Vector(pos) # this should be a vector
-        self.velocity = Vector(velocity) # this should be a vector
-        self.G = 6.674 * 10**-7
+        self.velocity = Vector(velocity) * mass # this should be a vector
+        self.G = 6.674 * 10**-30
+        self.sim_speed = sim_speed
 
     def find_attractions(self, bodies):
-        attractions = []
         for body in bodies:
             if body == self:
                 pass
             else:
                 attraction = self.G * self.mass * body.mass * self.pos.dist(body.pos)**-2
-                attractions.append(attraction)
                 self.velocity = self.velocity + self.pos.vdist(body.pos).norm() * attraction
+        #print(self.velocity.vect)
 
     def show(self, screen):
         self.pos = self.pos + self.velocity / self.mass
--- main.py
+++ main.py
@@ -11,18 +11,21 @@ pygame.init()
 size = width, height = (800, 600)
 screen = pygame.display.set_mode(size)  # this is the surface
 
+sim_speed = 10
 
 bodies = []
-sun = Body(2000000, 30, (255, 0, 0), [400, 300], [0, 0])
+sun = Body(2 * 10**30, 30, (255, 0, 0), [400, 300], [0, 0], sim_speed)
 bodies.append(sun)
-earth = Body(6, 10, (0, 255, 0), [300, 300], [0, -0.7])
+earth = Body(6 * 10**24, 3, (0, 255, 0), [300, 300], [0, -0.3], sim_speed)
 bodies.append(earth)
+asteroid = Body(0.07, 3, (127, 127, 127), [290, 300], [0, 0], sim_speed)
+#bodies.append(asteroid)
 
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
-    screen.fill((0, 0, 0))
+    #screen.fill((0, 0, 0))
     for i in bodies:
         i.find_attractions(bodies)
         i.show(screen)

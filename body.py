52f09c937117841a39f8ba5c622e25123c4cb3bd
bCubed3 <sf.bcubed@gmail.com>
2020-01-15 14:04:07 -0800

    bug fixes

--- body.py
+++ body.py
@@ -7,20 +7,22 @@ class Body:
         self.size = size
         self.color = color
         self.pos = Vector(pos) # this should be a vector
-        self.velocity = Vector(velocity) * mass # this should be a vector
-        self.G = 6.674 * 10**-30
+        self.velocity = Vector(velocity) # this should be a vector
+        self.G = 6.674 * 10**-11
         self.sim_speed = sim_speed
+        self.accel = Vector((0, 0))
 
     def find_attractions(self, bodies):
         for body in bodies:
             if body == self:
                 pass
             else:
-                attraction = self.G * self.mass * body.mass * self.pos.dist(body.pos)**-2
-                self.velocity = self.velocity + self.pos.vdist(body.pos).norm() * attraction
-        #print(self.velocity.vect)
+                attraction = self.G * self.mass * body.mass * (self.pos.dist(body.pos) * 10**9)**-2
+                #print("a :", attraction, "v :", self.velocity.vect)
+                self.velocity = self.velocity + (self.pos.vdist(body.pos).norm() * (attraction / self.mass))
 
     def show(self, screen):
-        self.pos = self.pos + self.velocity / self.mass
+        self.pos = self.pos + self.velocity
+        #print("c :", self.color, "v :", round(self.velocity).vect, "pos :", round(self.pos).vect)
         pygame.draw.circle(screen, self.color, round(self.pos), self.size)
 
--- main.py
+++ main.py
@@ -4,32 +4,58 @@ import sys
 import time
 from vector import Vector
 from body import Body
+import random
+import math
 
 user32 = ctypes.windll.user32
 pygame.init()
 # size = width, height = round(user32.GetSystemMetrics(0) / 1.3), round(user32.GetSystemMetrics(1) / 1.3)
-size = width, height = (800, 600)
+size = width, height = (800, 800)
 screen = pygame.display.set_mode(size)  # this is the surface
 
-sim_speed = 10
+sim_speed = 1
+size = 0
+c = (0, 0, 0)
+sx = 0
+sy = 0
 
 bodies = []
-sun = Body(2 * 10**30, 30, (255, 0, 0), [400, 300], [0, 0], sim_speed)
+sun = Body(2 * 10**30, 30, (255, 0, 0), [400, 400], [0, 0], sim_speed)
 bodies.append(sun)
-earth = Body(6 * 10**24, 3, (0, 255, 0), [300, 300], [0, -0.3], sim_speed)
+earth = Body(6 * 10**24, 5, (0, 255, 0), [150, 400], [0, 0.7], sim_speed)
 bodies.append(earth)
-asteroid = Body(0.07, 3, (127, 127, 127), [290, 300], [0, 0], sim_speed)
+super_earth = Body(6*10**26, 10, (0, 0, 255), [120, 300], [0.2, 0.4], sim_speed)
+bodies.append(super_earth)
+#asteroid = Body(0.07, 3, (127, 127, 127), [149.972, 400], [0, 0], sim_speed)
 #bodies.append(asteroid)
 
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
-    #screen.fill((0, 0, 0))
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            sx, sy = Vector(pygame.mouse.get_pos())
+            c = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
+        if event.type == pygame.MOUSEBUTTONUP:
+            x, y = pygame.mouse.get_pos()
+            if size > 30:
+                size = 30
+            size = 10**size
+            if sx - x == 0 and sy - y == 0:
+                bodies.append(Body(size, round(math.log(math.ceil(size), 10)) + 1, c, [sx, sy],
+                                   Vector([0, 0]).vect, sim_speed))
+            else :
+                bodies.append(Body(size, round(math.log(math.ceil(size), 10)) + 1, c, [sx, sy],
+                                   Vector([sx - x, sy - y]).norm().vect, sim_speed))
+            size = 0
+    screen.fill((0, 0, 0))
+    if pygame.mouse.get_pressed()[0] == 1:
+        size += 0.05
+        pygame.draw.circle(screen, c, [sx, sy], round(math.log(math.ceil(10**size), 10)) + 1)
     for i in bodies:
         i.find_attractions(bodies)
         i.show(screen)
-    print(sun.pos.vect)
+    #print(sun.pos.vect)
 
     pygame.display.flip()
     time.sleep(0.0001)
